52660f85ea228e607789a349f7ff26b110ffebe8
kimminji28 <dkdy78@naver.com>
2026-06-28 04:15:12 +0900

    김민지/코드값.그룹.소요시간 수정/260628

--- src/main/java/com/weple/cloud/auth/service/SignupService.java
+++
@@ -1,7 +0,0 @@
-package com.weple.cloud.auth.service;
-
-public interface SignupService {
-
-    // 회원가입 입력값을 검증하고 USERS 테이블에 가입 대기 계정을 생성합니다.
-    void signup(SignupRequestVO request);
-}
--- src/main/java/com/weple/cloud/history/task/service/TaskHistoryService.java
+++
@@ -1,6 +0,0 @@
-package com.weple.cloud.history.task.service;
-
-public interface TaskHistoryService {
-	void insertHistory(String taskId, String changedBy, String actionType, String oldTitle, String newTitle,
-            String oldTypeName, String newTypeName);
-}
